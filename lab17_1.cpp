72f3022086ef6506724e7b842ca791163a16939f
hearteiei <supalit1@gmail.com>
2021-02-19 17:33:42 +0700

    v.complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -20,21 +20,72 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename,vector<string> &names,vector<int> &scores,vector<char> &grades){
+ifstream h(filename.c_str());
+ string t;
+ while(getline(h,t)){
+char format[] ="%[^:]:%f %f %f";
+char name[100];
+float a,b,c;
+sscanf(t.c_str(),format,name,&a,&b,&c);
+char grade =score2grade(a+b+c);
+scores.push_back(a+b+c);
+names.push_back(name);
+grades.push_back(grade);
+ }
+}
+
+void getCommand(string &command,string &key){
+    cout <<"Please input your command: ";
+    cin >> command;
+    string g = toUpperStr(command);
+    if(g=="GRADE"||g=="NAME"){
+    cin.ignore();
+    getline(cin,key);
+    }
+
 
 }
 
-void getCommand(){
-
+void searchName( vector<string> names, vector<int> scores,vector<char> grades,string key){
+int n = names.size();
+int b=0;
+ for(int i=0;i<n;i++){
+    if(toUpperStr(names[i])==key.c_str()){
+        cout <<"---------------------------------\n";
+        cout << names[i]<<"'s score = "<<scores[i]<<"\n";
+        cout << names[i]<<"'s grade = "<<grades[i]<<"\n";
+        cout <<"---------------------------------\n";
+        b++;
+    }
+ }
+ if(b==0){
+     cout <<"---------------------------------\n";
+     cout << "Cannot found."<<"\n";
+     cout <<"---------------------------------\n";
+ }
 }
 
-void searchName(){
 
+void searchGrade(vector<string> names, vector<int> scores,vector<char> grades,string key){
+int n = names.size();
+int b=0;
+cout <<"---------------------------------\n";
+ for(int i=0;i<n;i++){
+     grades[i]=toupper(grades[i]);
+    if(grades[i]==key[0]){
+        cout << names[i]<<" "<<"("<<scores[i]<<")"<<"\n";
+        b++;
+    }
+ }
+ if(b==0){
+     cout <<"---------------------------------\n";
+     cout << "Cannot found."<<"\n";
+     cout <<"---------------------------------\n";
+ }
+  cout <<"---------------------------------\n";
 }
 
-void searchGrade(){
-
-}
 
 
 int main(){
